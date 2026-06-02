344237ffb45b948b38a62bb031bf95e982270c06
typing233 <helloe341@gmail.com>
2026-06-02 16:17:12 +0800

    Update CLI slides task artifacts

--- main_test.go
+++ main_test.go
@@ -187,10 +187,6 @@ func TestNumericBuffer(t *testing.T) {
 }
 
 func contains(s, sub string) bool {
-	return len(s) >= len(sub) && (s == sub || len(s) > 0 && containsHelper(s, sub))
-}
-
-func containsHelper(s, sub string) bool {
 	for i := 0; i <= len(s)-len(sub); i++ {
 		if s[i:i+len(sub)] == sub {
 			return true
